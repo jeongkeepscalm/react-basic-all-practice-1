ada78d9aca91e45b183fca7638a1b7055c24a8a0
JEONGGIL\ojg01 <jeongkeepscalm@gmail.com>
2025-03-03 14:27:43 +0900

    working 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
+import { useState } from "react";
+
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectedSelected.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState(
+    {
+      selectedProjectId: undefined,
+      projects: []
+    }
+  );
+
+  const handleStartAddProject = () => {
+    setProjectsState((prev) => {
+      // selectedProjectId: null 은 프로젝트 추가 상태를 의미한다. 
+      return {...prev, selectedProjectId: null};
+    });
+  }
+
+  const handleAddProject = (projectData) => {
+    setProjectsState((prev) => {
+
+      const newProject = {
+        id: Math.random(),
+        ...projectData
+      };
+
+      return {
+        ...prev,
+        projects: [...prev.projects, newProject]
+      };
+    });
+  }
+
+  console.log(projectsState.projects)
+
+  let content;
+
+  if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  } else if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}/>;
+  } 
+
   return (
     <>
-      <main className='h-screen my-8 flex gap-8'>
-        <ProjectsSidebar />
-        <NewProject />
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+        {content}
       </main>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+/*
+  React.StrictMode
+    개발 환경에서 컴포넌트가 2번 렌더링된다.
+    why?
+      잠재적인 버그를 감지하고, 예상치 못한 부작용을 방지하기 위해서
+*/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
